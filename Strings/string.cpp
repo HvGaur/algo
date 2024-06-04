bcd48a9dacba25ef98e0209b2b6dd0e34d969f76
HvGaur <hvgaurr@gmail.com>
2024-06-05 00:04:00 +0530

    d

--- Strings/string.cpp
+++ Strings/string.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main(){
-    
+    //Strings are dynamic arrays
+    string s("HELLO WORLD");
+    string t = "hello world";
+    cout << s << endl;
+    cout << t << endl;
+    string u; 
+    getline(cin, u, '.');
+    for(char ch: u){
+        cout << ch << " ";
+    }
 }
