8b8fec285470781a5284af3fe0ac1bf233f96dd9
HvGaur <hvgaurr@gmail.com>
2024-06-05 00:11:03 +0530

    string

--- Strings/stringvector.cpp
+++ Strings/stringvector.cpp
@@ -6,6 +6,16 @@ using namespace std;
 
 int main(){
     int n;
-    cin >> n
+    cin >> n;
+    string temp;
+    vector<string> vect;
+    cin.get();
+    while(n--){
+        getline(cin, temp);
+        vect.push_back(temp);
+    }
+    for(string s: vect){
+        cout << s << endl;
+    }
     return 0;
 }
