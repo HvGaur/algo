6262d589c7e030ee7db0d77f1aae9318fb788cab
HvGaur <hvgaurr@gmail.com>
2024-06-08 23:38:25 +0530

    matrix

---
+++ TwoDarray/initialisation.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+void print(int arr[][10], int m, int n){
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cout << arr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int arr[10][10];
+
+    int m,n;
+    cin >> m >> n;
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+    print(arr, m, n);
+}
