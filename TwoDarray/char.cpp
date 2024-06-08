cfcd9f832768f71480eb758351716c9561a13685
HvGaur <hvgaurr@gmail.com>
2024-06-08 23:49:25 +0530

    char array 2d

---
+++ TwoDarray/char.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char num[][10]= {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six"
+    };
+    cout << num[1];
+    // for(int i = 0; i < )
+}
