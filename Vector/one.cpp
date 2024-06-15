4a3f0f10c1b3f09ba07167c8e8dffc9a6554fea4
HvGaur <hvgaurr@gmail.com>
2024-06-15 12:17:15 +0530

    vectors

---
+++ Vector/one.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> arr = {1,2,3,4,5,6};
+
+    cout << arr.size() << endl;
+    cout << arr.capacity() << endl;
+
+    arr.push_back(7); //beyond capacity; capacity doubles, and reallocation happens
+
+    cout << arr.size() << endl;
+    cout << arr.capacity() << endl;
+
+    return 0;
+}
