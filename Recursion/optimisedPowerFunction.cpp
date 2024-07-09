c69f1c525d8dcfe1b091b990c636a8154ac887a7
HvGaur <hvgaurr@gmail.com>
2024-07-10 00:10:33 +0530

    optimisedPowerFunction

--- Recursion/optimisedPowerFunction.cpp
+++ Recursion/optimisedPowerFunction.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
-int main(){
+int optimisedPower(int a, int n){
+    if(n==0) return 1;
+    int sub = optimisedPower(a, n/2);
+    int subsq = sub*sub;
+    if(n&1) return a*subsq;
+    return subsq;
+}
 
+int main(){
+    int a = 2;
+    int n = 10;
+    cout << optimisedPower(a,n);
     return 0;
 }
