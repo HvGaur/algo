88c93b4a9c2f8b5915a8f0bfb99d70feeb5d424b
HvGaur <hvgaurr@gmail.com>
2024-07-09 23:47:01 +0530

    uh

--- Recursion/allOccurences.cpp
+++ Recursion/allOccurences.cpp
@@ -1,1 +1,17 @@
 // given a vector, find all the occurences of an integer k
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution{
+    public:
+    vector<int> getAllOccurences(vector<int> nums, int k){
+
+    }
+};
+
+int main(){
+    
+}
